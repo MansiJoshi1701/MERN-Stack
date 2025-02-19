87373a96453407ebb75a84d325a706ecaef4a2af
MansiJoshi1701 <mansijoshi.mj1798@gmail.com>
2025-02-19 12:16:45 -0800

    Added New Workout form

---
+++ frontend/src/components/WorkoutForm.js
@@ -0,0 +1,68 @@
+import { useState } from "react"
+
+const WorkoutForm = () => {
+    const [title , setTitle] = useState('')
+    const [load , setLoad] = useState('')
+    const [reps , setReps] = useState('')
+    const [error , setError] = useState(null)
+
+    const handleSubmit = async (e) => { //we're making this async coz we want to access db which'll take time
+        e.preventDefault()
+
+        const workout = {title , load , reps}
+
+        const response = await fetch('/api/workouts', {
+            method: 'POST',
+            body: JSON.stringify(workout),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+
+        const json = await response.json()
+
+        if(!response.ok){
+            setError(json.error)
+        }
+
+        if(response.ok){
+            setTitle('')
+            setLoad('')
+            setReps('')
+            setError(null)
+            console.log('new workout added' , json)
+        }
+    }
+
+    return(
+        <form className="create" onSubmit={handleSubmit}>
+            <h3>Add a new workout</h3>
+
+            <label>Excersize Title:</label>
+            <input 
+                type="text" 
+                onChange={(e) => setTitle(e.target.value)}
+                value={title} //2-way binding
+            />
+
+            <label>Load (in kg):</label>
+            <input 
+                type="number" 
+                onChange={(e) => setLoad(e.target.value)}
+                value={load} //2-way binding
+            />
+
+            <label>Reps:</label>
+            <input 
+                type="number" 
+                onChange={(e) => setReps(e.target.value)}
+                value={reps} //2-way binding
+            />
+
+            <button>Add workout</button>
+            {error && <div className="error">{error}</div>}
+        </form>
+    )
+}
+
+export default WorkoutForm
